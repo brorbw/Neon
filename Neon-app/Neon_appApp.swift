f5f9fb8e7bbc93aabdee3ed06684359b32447678
Bror Winther <git@nobad.coffee>
2024-09-02 02:12:41 +0200

    Initial Commit

---
+++ Neon-app/Neon_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neon_appApp.swift
+//  Neon-app
+//
+//  Created by nah on 02/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Neon_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
